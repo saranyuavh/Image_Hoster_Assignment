3e95ab6e4c899afdc993e02d41cf3d8fadefb73a
saranyuavh <saranyu38@gmail.com>
2021-01-28 23:18:53 +0530

    Password Strength feature added

--- src/main/java/ImageHoster/controller/UserController.java
+++ src/main/java/ImageHoster/controller/UserController.java
@@ -13,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
-
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Controller
 public class UserController {
@@ -40,7 +42,18 @@ public class UserController {
     //This controller method is called when the request pattern is of type 'users/registration' and also the incoming request is of POST type
     //This method calls the business logic and after the user record is persisted in the database, directs to login page
     @RequestMapping(value = "users/registration", method = RequestMethod.POST)
-    public String registerUser(User user) {
+    public String registerUser(User user, RedirectAttributes redirectAttrs) {
+        String REGEX = "^(?=(.*[a-zA-Z].*))(?=.*\\d.*)(?=.*\\W.*)[a-zA-Z0-9\\S]{1,200}$";
+        String password = user.getPassword();
+
+        Pattern pattern = Pattern.compile(REGEX);
+        Matcher matcher = pattern.matcher(password);
+        if (!matcher.matches()){
+            String error = "Password must contain atleast 1 alphabet, 1 number & 1 special character";
+            redirectAttrs.addAttribute("passwordTypeError", error).addFlashAttribute("passwordTypeError", error);
+            return "redirect:/users/registration";
+
+        }
         userService.registerUser(user);
         return "redirect:/users/login";
     }
